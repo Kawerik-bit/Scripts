eb01c46706b66a50967351856a95b20b3367e915
Kawerik-bit <krisleonn@gmail.com>
2024-10-14 11:57:52 +0300

    Update Sys-update.sh

--- Scripts-bash/Sys-update.sh
+++ Scripts-bash/Sys-update.sh
@@ -15,3 +15,6 @@ if [ -f /var/run/reboot-required ]; then
     echo 'Please Reboot!'
     reboot now
 fi
+
+#execute sith "./Sys-update.sh"
+# execute it while being in the directory of the script or specify the path to it
